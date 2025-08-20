113dcc9a0c6c2ea89b723600f644cfebf268ffc3
revy <revy@cnt.id>
2025-08-20 11:25:50 +0700

    Add data dummy

---
+++ data/user.js
@@ -0,0 +1,9 @@
+export const user = [
+    {
+        name: "Muhammad Revy",
+        uid: "01100830333",
+        username: "muhammadrevy",
+        email: "rmuhammadrevy@gmail.com",
+        password: "080806",
+    }
+];
